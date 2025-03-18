38ddb31929faa6c26b8f5b00f5647f205d5c2dc4
jfelipefg17 <jfelipefg17@gmail.com>
2025-03-18 14:01:36 -0500

    ready for merge with dev

--- projectBookStore/src/main/java/com/BookStore/projectBookStore/controllers/BookController.java
+++ projectBookStore/src/main/java/com/BookStore/projectBookStore/controllers/BookController.java
@@ -147,6 +147,7 @@ public class BookController {
                 modelMap.addAttribute("error", "No se encontró un libro con ese nombre.");
             }
         }
+        
         return "findByName";
     }
 
